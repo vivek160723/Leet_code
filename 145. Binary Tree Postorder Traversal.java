2a78d30f97833a2997b97aa91914161e440241fb
vivek <vivekchaudhary160723@gmail.com>
2024-04-18 08:09:23 +0530

    Create 145. Binary Tree Postorder Traversal.java

---
+++ 145. Binary Tree Postorder Traversal.java
@@ -0,0 +1,13 @@
+class Solution {
+    ArrayList<Integer> result=new ArrayList<>();
+    public List<Integer> postorderTraversal(TreeNode root) {
+        if (root==null){
+        return result;
+        }
+
+        postorderTraversal(root.left);
+        postorderTraversal(root.right);
+        result.add(root.val);
+            return result;
+    }
+}
