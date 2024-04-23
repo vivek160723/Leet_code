b6afac243ec612d71dace3e9010e730be6d88118
vivek chaudhary <vivekchaudhary160723@gmail.com>
2024-04-23 15:36:23 +0530

    Create 107. Binary Tree Level Order Traversal II.java

---
+++ 107. Binary Tree Level Order Traversal II.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null)
+            return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> level = new ArrayList<>();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null)
+                    queue.offer(node.left);
+                if (node.right != null)
+                    queue.offer(node.right);
+            }
+
+            result.add(level);
+        }
+
+        Collections.reverse(result);
+        return result;
+    }
+}
