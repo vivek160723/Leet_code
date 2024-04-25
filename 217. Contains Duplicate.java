1b1cc03fb441ccc783133bced6b1a03d15f362bf
vivek chaudhary <vivekchaudhary160723@gmail.com>
2024-04-25 10:31:51 +0530

    Create 217. Contains Duplicate.java

---
+++ 217. Contains Duplicate.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int num:nums){
+        if(map.containsKey(num) && map.get(num)>=1)
+        return true;
+        map.put(num,map.getOrDefault(num,0)+1);
+        }
+        return false;
+    }
+}
