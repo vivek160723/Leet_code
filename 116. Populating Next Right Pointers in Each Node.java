941b6e9c88f2e88ba706b665fa59f228b81698d9
vivek chaudhary <vivekchaudhary160723@gmail.com>
2024-04-28 14:06:56 +0530

    Update 116. Populating Next Right Pointers in Each Node.java

--- 116. Populating Next Right Pointers in Each Node.java
+++ 116. Populating Next Right Pointers in Each Node.java
@@ -1,3 +1,4 @@
+//===================iterative approach using queue===================
 class Solution {
     public Node connect(Node root) {
         if(root==null) return null;
@@ -28,3 +29,34 @@ class Solution {
         return root;
 }
 }
+
+
+//===================================Recusrsive approach============================
+
+class Solution
+{
+  public Node connect (Node root)
+  {
+	if (root == null)
+	  return null;
+	if (root.left != null)
+	  {
+		root.left.next = root.right;
+	  }
+	if (root.right != null && root.next != null)
+	  {
+		root.right.next = root.next.left;
+	  }
+	connect (root.left);
+	connect (root.right);
+	return root;
+  }
+}
+
+
+
+
+
+
+
+//
