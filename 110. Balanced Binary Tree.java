d7e69b03ae8c95bca20197b84afe4fdb0cc87e24
vivek chaudhary <vivekchaudhary160723@gmail.com>
2024-07-27 10:38:15 +0530

    Create 110. Balanced Binary Tree.java

---
+++ 110. Balanced Binary Tree.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return height(root) != -1;
+    }
+
+    private int height(TreeNode node) {
+        if (node == null) return 0;
+
+        int leftHeight = height(node.left);
+        if (leftHeight == -1) return -1;
+
+        int rightHeight = height(node.right);
+        if (rightHeight == -1) return -1;
+
+        if (Math.abs(leftHeight - rightHeight) > 1) return -1;
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
